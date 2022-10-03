9da376a9bd648ef96dc963d42cddd2353521a625
Kathryn Tsai <katalystkat@Kathryns-Air.attlocal.net>
2022-10-03 10:27:37 -0700

    need to update reducer functions

--- client/actions/actions.js
+++ client/actions/actions.js
@@ -1,21 +1,52 @@
 // import all actionType constants
 import * as types from '../constants/actionTypes.js';
+import axios from 'axios';
 
-export const addSpice = data => ({
-  type: types.ADD_SPICE,
-  payload: data
+export const addSpice = spiceInfo => (dispatch, getState) => (
+    axios.put('/spices', {spiceInfo}
+        .then({
+            type: types.ADD_SPICE,
+            payload: data,
+        })
+        .catch(console.log(error),
+        )
+    )
+);
+
+export const deleteSpice = spiceInfo => (dispatch, getState) => {
+    axios.delete('/spices', {spiceInfo}
+        .then({
+            type: types.DELETE_SPICE,
+            payload: data.id,
+        })
+        .catch(console.log(error),
+        ))
+};
+
+export const updateAmount = spiceInfo => (dispatch, getState) => {
+    axios.patch('/spices', {spiceInfo}
+        .then({
+            type: types.UPDATE_AMOUNT,
+            payload: data.id,
+        })
+        .catch(console.log(error),
+        ))
 });
 
-export const deleteSpice = data => ({
-  
-});
+// export const syncSpices = data => ({
+//   axios.put('/spices', getState().
+// })
 
-export const updateAmount = data => ({
-
-});
-
-
-
+export const getSpices = () => (dispatch) => {
+    axios.get('/spices')
+        .then(({data}) => {
+            dispatch({
+                type: types.GET_SPICES,
+                payload: data,
+            })
+        })
+        .catch(console.log(error));
+}
 
 
 
--- client/reducers/spicesReducer.js
+++ client/reducers/spicesReducer.js
@@ -14,5 +14,37 @@ import * as types from '../constants/actionTypes';
 const initialState = {
     totalSpices: 0,
     spiceRack: [],
-    
-}
+    newSpice: '',
+    synced: true,
+};
+const spicesReducer = (state = initialState, action) => {
+    switch(action.type){
+        case types.ADD_SPICE:
+            return{
+                ...state,
+                spiceRack: state.spiceRack.concat({
+                    name: action.payload.name,
+                    size: action.payload.size,
+                    amount: action.payload.amount,
+                }),
+                totalSpices: state.totalSpices + 1,
+                synced: false,
+            };
+        case types.DELETE_SPICE:
+            
+            // iterate through array, for Each object,  if id = xyz, delete object, and assign state to new updated spicerack.
+            return{
+                ...state,
+                spiceRack: [],
+                totalSpices = state.totalSpices - 1,
+                synced: false,
+            }
+        case types.UPDATE_AMOUNT:
+            return{
+                ...state,
+                spiceRack : 
+            }
+    }
+};
+
+export default spicesReducer;
