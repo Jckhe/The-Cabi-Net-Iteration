470de8c11535ffd33d35f44a9be2b631a3796d88
Zoe Harper <zoeharper@zoes-mbp.lan>
2022-10-04 19:30:08 -0700

    mild tweaks to functionality, added missing action constant

--- client/store.js
+++ client/store.js
@@ -1,13 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './reducers/index';
-import { applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
 
 const store = configureStore(
     reducers,
-    composeWithDevTools(applyMiddleware(thunk)),
+    composeWithDevTools,
     );
 
 export default store
