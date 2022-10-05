f52fdf42fbf50c02b61d400c38f3f2cc75047322
Andrew <andrewngo91@gmail.com>
2022-10-04 20:01:07 -0700

    login - need to test

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -15,32 +15,6 @@ export function LoginPage (props) {
 //deconstruct the props
   const { loginHandler } = props;
 
-  //saves the username
-  // (`/api/${event.target.value}`,
-  function handleClick(event) {
-    let action;
-    if (signup) action = 'signup'
-    else action = 'login'
-    let url = '/api/' + action
-    fetch(url, {
-      method: 'POST',
-      credentials: 'include',
-      headers: {
-        'Accept': "application/json, text/plain",
-        'Content-Type': 'application/json',
-        'x-Trigger': 'CORS'
-        },
-        body: JSON.stringify({username: username, password: password})
-    })
-      .then((res) => {
-        if (res.status === 200 || res.status === 201) {
-          console.log(res)
-          loginHandler(username);
-          return res;
-        }
-      });
-  };
-
   //login/signup modals
   return (
     <div className ="main">
@@ -52,7 +26,7 @@ export function LoginPage (props) {
           <input type = "password" placeholder="Password" value={password} onChange={(e) => {setPassword(e.target.value)}} required/>
         </div>
         <div className="loginSubmitContainer">
-          <input type="submit" onClick={() => {handleClick()}} className="loginButton"/>
+          <input type="submit" onClick={() => {loginHandler()}} className="loginButton"/>
         </div>
         { signup 
           ? <div> <p>Already have an account?</p><button onClick={() => {toggleSignUp(false)}}>Return to Login</button> </div> 
